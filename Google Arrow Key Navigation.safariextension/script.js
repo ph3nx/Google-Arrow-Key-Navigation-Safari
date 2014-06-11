0157270d62a1a9ea598fd652b716e64b926341db
Pascal Gläser <pasi1995@gmail.com>
2014-06-11 13:34:00 +0200

    fix preventDefault()

--- Google Arrow Key Navigation.safariextension/script.js
+++ Google Arrow Key Navigation.safariextension/script.js
@@ -1,11 +1,12 @@
 document.getElementById('gsr').onkeydown = function(e) {
 	key = window.event? event.keyCode: e.keyCode;
 	if (key) {
-		e.preventDefault();
 		if (key == 39) {
 			document.getElementById('pnnext').click();
+			e.preventDefault();
 		} else if (key == 37) {
 			document.getElementById('pnprev').click();
+			e.preventDefault();
 		}
 	} 
 }
